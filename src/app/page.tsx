059e6412cb907edc4a81303e558e733bf1605384
Ajay Kumar <ajay@sprouts.ai>
2026-03-06 14:50:51 +0530

    02-dabase-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany();
   return (
-    <div className="h-screen w-screen justify-center items-center flex">
-      <Button>Button</Button>
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {posts.map((post) => (
+          <li key={post.id} className="mb-2">
+            {post.title}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
