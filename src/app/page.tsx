1fa0fe5a4b13f7ae3b791cf00ddd2d3b2bff22f7
Ajay Kumar <ajay@sprouts.ai>
2026-03-06 16:00:59 +0530

    feat: set up tRPC for type-safe API communication and data fetching across client and server components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-import prisma from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+import { Suspense } from "react";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany();
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.hello.queryOptions({ text: "Ajay & Alka" }),
+  );
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
-        Superblog
-      </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {posts.map((post) => (
-          <li key={post.id} className="mb-2">
-            {post.title}
-          </li>
-        ))}
-      </ol>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
